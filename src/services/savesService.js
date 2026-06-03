67f254c5102634069fa529b159f6e22960b251c1
arrgn <cemas045@vk.com>
2026-06-03 15:23:23 +0300

    hotfix: typo

--- src/services/savesService.js
+++ src/services/savesService.js
@@ -24,7 +24,7 @@ const create = (saveData) => {
 
   const newId = saves.length > 0 ? Math.max(...saves.map((s) => s.id)) + 1 : 1;
 
-  const newStock = { id: newId, ...saveData };
+  const newStock = { id: newId, ...saveData, src: `./public/${newId}.jpg`, desc_src: `./public/${newId}-1.jpg` };
   saves.push(newStock);
   fileService.writeData(dataFilePath, saves);
 
