de79052266f3d580135c853ab2a48a2a2751af78
arrgn <cemas045@vk.com>
2026-06-05 06:25:51 +0300

    Added calculator to vite build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+import { resolve } from "node:path";
+
 export default {
   build: {
     outDir: "./build",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        calculator: resolve(__dirname, "calculator.html"),
+      },
+    },
   },
 };
